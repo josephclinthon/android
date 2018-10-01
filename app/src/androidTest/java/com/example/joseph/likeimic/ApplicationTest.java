7a919f6bc90b50766d03d8e72559956d15768029
josephclinthon <josephclinthon@gmail.com>
2018-09-30 21:37:01 -0500

    Prueba para la tarea de CAS

--- app/src/androidTest/java/com/example/joseph/likeimic/ApplicationTest.java
+++ app/src/androidTest/java/com/example/joseph/likeimic/ApplicationTest.java
@@ -8,6 +8,7 @@ import android.test.ApplicationTestCase;
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
+//comentario para github
         super(Application.class);
     }
 }
